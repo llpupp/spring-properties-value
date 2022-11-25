59e8a7617532eec38ca0e9ad24c1b54aa8721fc7
llpupp <llpupp@hotmail.com>
2022-11-25 11:38:45 -0300

    Initial commit

---
+++ src/main/java/dev/projetos/springpropertiesvalue/app/SistemaMensagem.java
@@ -0,0 +1,28 @@
+package dev.projetos.springpropertiesvalue.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SistemaMensagem implements CommandLineRunner {
+    @Value("${name:NoReply-DIO}") // no app-properties foi usado nome e aqui o "name", então usado NoReply-DIO caso não encontre a propriedade
+    private String nome;
+    @Value("${email}")
+    private String email;
+    @Value("${telefones}")
+    private List<Long> telefones =
+            new ArrayList<>(Arrays.asList(new Long[]{11956781254L}));
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Mensagem enviada por: " + nome
+                + "\nE-mail:" + email
+                + "\nCom telefones para contato: " + telefones);
+        System.out.println("Seu cadastro foi aprovado");
+    }
+}
---
+++ src/test/java/dev/projetos/springpropertiesvalue/SpringPropertiesValueApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projetos.springpropertiesvalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrimeirosPassosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
